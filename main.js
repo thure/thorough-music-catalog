872785496fbcc5e4c73e4d95cba9b531d18b39bf
Will Shown <w@willshown.com>
2015-12-01 21:22:21 -0800

    Adding electron boilerplate.

---
+++ main.js
@@ -0,0 +1,26 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+electron.crashReporter.start();
+
+var mainWindow = null;
+
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('ready', function() {
+
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+
+});
